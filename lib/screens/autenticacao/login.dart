a7c9309b192f14e9f69b2feb81cb409695593fe1
pedro <pedromoraesmeter@gmail.com>
2022-01-26 20:18:22 -0300

    formulário em widget

--- lib/screens/autenticacao/login.dart
+++ lib/screens/autenticacao/login.dart
@@ -11,9 +11,7 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme
-          .of(context)
-          .primaryColor,
+      backgroundColor: Theme.of(context).primaryColor,
       body: Padding(
         padding: const EdgeInsets.only(top: 30),
         child: SingleChildScrollView(
@@ -40,106 +38,7 @@ class Login extends StatelessWidget {
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Faça o seu Login:',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: 'CPF',
-                          ),
-                          maxLength: 11,
-                          keyboardType: TextInputType.number,
-                          controller: _cpfController,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: 'Senha',
-                          ),
-                          maxLength: 16,
-                          controller: _passwordController,
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: OutlinedButton(
-                            onPressed: () {
-                              if (_validaCampos()) {
-                                Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => Dashboard(),
-                                    ),
-                                        (route) => false);
-                              } else {
-                                showDialog(context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: const Text('Atenção'),
-                                        content: const Text(
-                                            'CPF ou Senha inválidos'),
-                                        actions: [
-                                          TextButton(onPressed: () {
-                                            Navigator.pop(context);
-                                          }, child: const Text('Fechar'),),
-                                        ],
-                                      );
-                                    });
-                              }
-                            },
-                            child: const Text('Continuar'),
-                            style: OutlinedButton.styleFrom(
-                              primary: Colors.green[900],
-                              side: BorderSide(
-                                  color: Theme
-                                      .of(context)
-                                      .primaryColor,
-                                  width: 2),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Esqueci minha senha',
-                          style: TextStyle(
-                            color: Theme
-                                .of(context)
-                                .primaryColor,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            primary: Colors.green[900],
-                            side: BorderSide(
-                                color: Theme
-                                    .of(context)
-                                    .primaryColor,
-                                width: 2),
-                          ),
-                          onPressed: () {},
-                          child: const Text('Registrar'),
-                        ),
-                      ],
-                    ),
+                    child: _construirFormulario(context),
                   ),
                 ),
               ),
@@ -158,10 +57,98 @@ class Login extends StatelessWidget {
     }
   }*/
   Widget _construirFormulario(context) {
-    return
-      Form(
-        child:,
-        key: _formKey,
-      );
+    return Form(
+      child: Column(
+        children: [
+          const Text(
+            'Faça o seu Login:',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'CPF',
+            ),
+            maxLength: 11,
+            keyboardType: TextInputType.number,
+            controller: _cpfController,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Informe o CPF';
+              }
+              if (value.length < 11) {
+                return 'CPF inválido';
+              }
+            },
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Senha',
+            ),
+            maxLength: 16,
+            controller: _passwordController,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Informe uma Senha';
+              }
+            },
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: OutlinedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Dashboard(),
+                      ),
+                      (route) => false);
+                }
+              },
+              child: const Text('Continuar'),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.green[900],
+                side:
+                    BorderSide(color: Theme.of(context).primaryColor, width: 2),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            'Esqueci minha senha',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              primary: Colors.green[900],
+              side: BorderSide(color: Theme.of(context).primaryColor, width: 2),
+            ),
+            onPressed: () {},
+            child: const Text('Registrar'),
+          ),
+        ],
+      ),
+      key: _formKey,
+    );
   }
 }
